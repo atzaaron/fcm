ca3bb8c1fecfd57e94c7348368341de9cea7f7ee
ATzAaron <ATzAaron@localhost.localdomain>
2020-10-14 04:15:06 +0900

    feat (result_ui): Modified UI of Resultats page. Added 2 dropdowns menu in order to select options (in future)

--- lib/results.dart
+++ lib/results.dart
@@ -15,6 +15,8 @@ class Results extends StatefulWidget {
 class _ResultsState extends State<Results> {
 
   List<String> clubName = [];
+  int _team = 1;
+  int _competition = 1;
 
   void initState() {
     super.initState();
@@ -47,16 +49,15 @@ class _ResultsState extends State<Results> {
   Widget build(BuildContext build) {
     if (this.clubName.length > 0) {
       return (
-        // Scaffold (
-          /*body:*/ Container(
-            height: 300,
-            child: ListView.builder(
-              itemCount: this.clubName.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Text(this.clubName[index]);
-              },
-            ),
-          )
+        this.setChoicesTab()
+        // Container(
+        //   height: 300,
+        //   child: ListView.builder(
+        //     itemCount: this.clubName.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       return Text(this.clubName[index]);
+        //     },
+        //   ),
         // )
       );      
     }
@@ -81,5 +82,84 @@ class _ResultsState extends State<Results> {
       return false;
     }
     return double.tryParse(s) != null;
+  }
+
+  Column setChoicesTab() {
+    return (
+      Column(
+        children: [
+          Container(
+            width: double.infinity,
+            color: Color(0xff0950b5),
+            child: Center(
+              child: Theme(
+                data: Theme.of(context).copyWith(canvasColor: Color(0xff0950b5)),
+                child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.white
+                      ),
+                      value: _team,
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Équipe 1", style: TextStyle(color: Colors.white)),
+                          value: 1,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Équipe 2", style: TextStyle(color: Colors.white)),
+                          value: 2,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Équipe 3", style: TextStyle(color: Colors.white)),
+                          value: 3
+                        )
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _team = value;
+                        });
+                      }
+                    ),                  
+                  )
+              )
+
+            )
+          ),
+          Divider(
+            color: Colors.white,
+            height: 0.1,
+          ),
+          Container(
+            width: double.infinity,
+            color: Color(0xff0950b5),
+            child: Center(
+              child: IgnorePointer(
+                ignoring: true,
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.white
+                    ),
+                    value: _competition,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Championnat", style: TextStyle(color: Colors.white)),
+                        value: 1,
+                      )
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        _competition = value;
+                      });
+                    }),
+                  ),
+              )
+            )
+          )
+        ],
+      )
+    );
   }
 }
